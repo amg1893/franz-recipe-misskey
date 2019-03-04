e61a8881b4e73400674074fb74296c1ce48129fd
Adrián Mora <amora@adrianmora.me>
2019-03-04 23:24:47 +0100

    well, now it works

--- webview.js
+++ webview.js
@@ -1,19 +1,8 @@
-const { ipcRenderer } = require('electron');
-
-// Tweetdeck redirect fix
-ipcRenderer.on('redirect-url', (event, url) => {
-  window.location.assign(url);
-});
-
 module.exports = (Franz) => {
   const getMessages = function getMessages() {
-    const elements = document.querySelectorAll('.msg-unread-count');
-    let count = 0;
-    if (elements[0]) {
-      count = parseInt(elements[0].innerHTML, 10);
-    }
+    let elements = /\((\d+)\).*$/ig.exec(document.title);
 
-    Franz.setBadge(count);
+    Franz.setBadge('1' in elements ? elements[1] : 0);
   };
   Franz.loop(getMessages);
 };
